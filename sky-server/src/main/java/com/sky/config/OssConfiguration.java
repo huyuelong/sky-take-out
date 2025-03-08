4e899fa30b0c11e6d10a78b18171af1acb51553b
huyuelong <949910047@qq.com>
2025-03-08 23:11:00 +0800

    新增菜品业务代码开发

---
+++ sky-server/src/main/java/com/sky/config/OssConfiguration.java
@@ -0,0 +1,32 @@
+package com.sky.config;
+
+import com.sky.properties.AliOssProperties;
+import com.sky.utils.AliOssUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 配置类，用于创建AliOssUtil对象
+ */
+@Configuration
+@Slf4j
+public class OssConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public AliOssUtil aliOssUtil(AliOssProperties aliOssProperties) {
+        if (aliOssProperties == null) {
+            log.error("AliOssProperties 配置未正确加载，文件上传功能可能无法使用！");
+            throw new RuntimeException("AliOssProperties 配置未正确加载");
+        }
+
+        log.info("开始创建阿里云文件上传工具类对象，配置信息：{}", aliOssProperties);
+        return new AliOssUtil(aliOssProperties.getEndpoint(),
+                aliOssProperties.getAccessKeyId(),
+                aliOssProperties.getAccessKeySecret(),
+                aliOssProperties.getBucketName());
+    }
+}
+
